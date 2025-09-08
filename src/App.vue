e52cd88b9f1ead92041281a95053adb23df27984
Springer <zxllks@yahoo.com>
2025-09-09 07:57:48 +0800

    03: try tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1 class="bg-sky-300 text-3xl font-bold underline">Hello world!</h1>
+  <div class="flex h-screen items-center justify-between">
+    <div class="h-full w-[10rem] bg-gray-300 text-gray-700 hover:bg-yellow-300">
+      sidebar
+    </div>
+
+    <div class="h-full flex-1 bg-green-300 text-blue-700">
+      content
+      <input
+        type="text"
+        placeholder="Search for anything"
+        class="placeholder:text-blue-300"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
