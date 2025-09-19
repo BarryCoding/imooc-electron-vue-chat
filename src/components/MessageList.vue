58f47a3fc1e9112288c1fcb8ec3561533e45c5f1
Springer <zxllks@yahoo.com>
2025-09-19 08:05:57 +0800

    14-2: add tailwind Typography plugin

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -34,9 +34,12 @@ defineProps<{ messages: MessageProps[] }>();
             <template v-if="message.status === 'loading'">
               <Icon icon="eos-icons:three-dots-loading"></Icon>
             </template>
-            <template v-else>
+            <div
+              v-else
+              class="prose prose-slate prose-headings:my-2 prose-p:my-1 prose-ul:my-1 prose-li:my-0 prose-hr:my-1"
+            >
               <VueMarkdown :source="message.content" />
-            </template>
+            </div>
           </div>
         </div>
       </div>
