5a0f1b21be3d20deb60794ac6606299acd17b3c4
Springer <zxllks@yahoo.com>
2025-09-20 10:29:58 +0800

    15-1: file-upload and preview

--- src/components/MessageInput.vue
+++ src/components/MessageInput.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import Button from "./Button.vue";
+import { Icon } from "@iconify/vue";
 
 defineProps<{
   disabled?: boolean;
@@ -8,7 +10,23 @@ defineProps<{
 const emit = defineEmits<{
   create: [value: string];
 }>();
+
 const userMessage = defineModel<string>();
+const imagePreview = ref("");
+
+const handleImageUpload = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.files && target.files.length > 0) {
+    const selectedImage = target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(selectedImage);
+    reader.onload = (e) => {
+      if (e.target?.result) {
+        imagePreview.value = e.target.result as string;
+      }
+    };
+  }
+};
 const onCreate = () => {
   if (userMessage.value && userMessage.value?.trim() !== "") {
     emit("create", userMessage.value);
@@ -18,20 +36,50 @@ const onCreate = () => {
 
 <template>
   <div
-    class="message-input flex w-full items-center rounded-lg border border-gray-300 px-2 py-1 shadow-sm focus-within:border-green-700"
+    class="message-input w-full rounded-lg border border-gray-300 px-2 py-1 shadow-sm focus-within:border-green-700"
   >
-    <input
-      class="flex-1 border-0 bg-white outline-none focus:ring-0"
-      type="text"
-      v-model="userMessage"
-      :disabled="disabled"
-    />
-    <Button
-      icon-name="radix-icons:paper-plane"
-      @click="onCreate"
-      :disabled="disabled"
-    >
-      发送
-    </Button>
+    <div v-if="imagePreview" class="relative mb-2 flex items-center">
+      <img
+        :src="imagePreview"
+        alt="upload image preview"
+        class="h-24 w-24 rounded object-cover"
+      />
+    </div>
+    <div class="flex items-center">
+      <label>
+        <Icon
+          icon="radix-icons:image"
+          width="24"
+          height="24"
+          class="mr-2"
+          :class="[
+            disabled
+              ? 'cursor-not-allowed text-gray-300'
+              : 'cursor-pointer text-gray-400 hover:text-gray-600',
+          ]"
+        />
+        <input
+          type="file"
+          accept="image/*"
+          class="hidden"
+          @change="handleImageUpload"
+          :disabled="disabled"
+        />
+      </label>
+
+      <input
+        class="flex-1 border-0 bg-white outline-none focus:ring-0"
+        type="text"
+        v-model="userMessage"
+        :disabled="disabled"
+      />
+      <Button
+        icon-name="radix-icons:paper-plane"
+        @click="onCreate"
+        :disabled="disabled"
+      >
+        发送
+      </Button>
+    </div>
   </div>
 </template>
